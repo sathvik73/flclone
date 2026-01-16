ad1a8a4382d0c7698b08202f36467fe6b23f5074
Gaddala Sathvik <118980526+sathvik73@users.noreply.github.com>
2026-01-16 19:00:15 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -6,7 +6,20 @@ dotenv.config();
 let pool;
 
 if (process.env.DATABASE_URL) {
-    pool = mysql.createPool(process.env.DATABASE_URL);
+    console.log('Connecting to remote MySQL...');
+    const dbUrl = new URL(process.env.DATABASE_URL);
+    const config = {
+        host: dbUrl.hostname,
+        user: dbUrl.username,
+        password: dbUrl.password,
+        database: dbUrl.pathname.slice(1),
+        port: dbUrl.port ? parseInt(dbUrl.port) : 3306,
+        ssl: {
+            minVersion: 'TLSv1.2',
+            rejectUnauthorized: true
+        }
+    };
+    pool = mysql.createPool(config);
 } else {
     pool = mysql.createPool({
         host: process.env.DB_HOST || 'localhost',
